a2050a3ed093af9114b79f1bf791c1181782bf7d
Bandana singh <bandana.singh@techaspect.com>
2017-07-19 00:37:40 +0530

    https://github.com/Bandana9Singh/single-page-seed-app.git

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,5 +9,6 @@ export const mainRouting = RouterModule.forRoot([
     { path: '', component: HomeComponent },
     { path: 'posts', component: PostsComponent },
     { path: 'users', component: UsersComponent },
-    { path: 'users/new', component: FormComponent }
+    { path: 'users/new', component: FormComponent },
+    { path: 'users/:id', component: FormComponent }
 ]);
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,28 +1,60 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { EmailValidators } from './common/email.validators';
+import { UserService } from '../users/users.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers: [UserService]
 })
-export class FormComponent {
-  
-  form = new FormGroup({
-    name: new FormControl('',[
-      Validators.required,
-      Validators.minLength(5)
-      ]),
-    email: new FormControl('', Validators.required)
-  });
+export class FormComponent implements OnInit{
+  form : FormGroup ;
+  users;
 
+  constructor(fb: FormBuilder, private _route: ActivatedRoute, private _userService: UserService){
+    
+    this.form = fb.group({
+      name: ['',Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ])],
+      email: ['', Validators.compose([
+        Validators.required,
+        EmailValidators.invalidEmail
+      ])],
+      phone: [],
+      address: fb.group({
+        street: [],
+        suite: [],
+        city: [],
+        zipcode: []
+      })
+    });
+  }
+
+  ngOnInit() {
+    this._route.paramMap.subscribe(
+      params => {
+        console.log(+params.get('id'));
+      }
+    );
+
+    this._userService.getUsers().subscribe(users=>{
+      this.users = users;
+    });
+  }
   get name(){
     return this.form.controls.name;
   }
   get email(){
     return this.form.controls.email;
   }
-  onClick(){
+  save(){
     console.log(this.form);
   }  
 }
